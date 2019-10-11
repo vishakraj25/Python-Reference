6e1767cbf940b43d94c1eacdc51682cc7bee92b9
Shanmugaraj Kamaraj <33391433+vishakraj25@users.noreply.github.com>
2019-10-11 16:44:21 +0530

    Add files via upload

---
+++ pack/module1.py
@@ -0,0 +1,11 @@
+"""
+Package p
+module 1
+
+"""
+
+def hello():
+    print("hello")
+
+def sub(x,y):
+    return(x-y)
---
+++ pack/module2.py
@@ -0,0 +1,13 @@
+"""
+
+Package p
+module 2
+
+"""
+
+
+def greetings():
+    print("greetings")
+
+def mul(x,y):
+    return(x*y)
